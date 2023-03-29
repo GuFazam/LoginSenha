8425c47ff1f3fb8d887f7cd6e2e6e4b5225cd4f9
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-03-29 20:12:17 -0300

    Adicionar arquivos de projeto.

---
+++ LoginSenha/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace LoginSenha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (textBoxUsuario.Text == "Admin" && textBoxSenha.Text == "123456")
+            {
+                MessageBox.Show("Bem vindo ao sistema!", "Acesso ao Sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Acesso Negado!", "Acesso ao Sistema!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}

5f104c73110f06f062ecf564386a5717780dec37
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-03-29 21:42:04 -0300

    usei threading para fechar o Form1 e abrir em seguida o Form2

--- LoginSenha/Form1.cs
+++ LoginSenha/Form1.cs
@@ -13,6 +13,7 @@ namespace LoginSenha
 {
     public partial class Form1 : Form
     {
+        Thread nt; //criação do thread como 'se fosse uma variável'
         public Form1()
         {
             InitializeComponent();
@@ -27,12 +28,23 @@ namespace LoginSenha
         {
             if (textBoxUsuario.Text == "Admin" && textBoxSenha.Text == "123456")
             {
-                MessageBox.Show("Bem vindo ao sistema!", "Acesso ao Sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //MessageBox.Show("Bem vindo ao sistema!", "Acesso ao Sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //essa parte acima foi para exemplificar o acesso ao sistemas com usuário já "cadastrado"
+                this.Close(); //neste caso iremos fechar o primeiro formulário e iremos já para o seguinte formulário
+                nt = new Thread(Form2);
+                nt.SetApartmentState(ApartmentState.STA);
+                nt.Start();
             }
             else
             {
                 MessageBox.Show("Acesso Negado!", "Acesso ao Sistema!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
+        }
+
+        private void Form2(object obj)
+        {
+            // throw new NotImplementedException(); código criado pois apenas vamos criar o novo formulário e instância-lo
+            Application.Run(new Form2()); //está linha serve para abrir o Form2 após clicar no botão login
         }
     }
 }
